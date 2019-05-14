b1fb31f6a458ee3d805b72314962fcf097d4c020
Steve Lorek <steve@stevelorek.com>
2019-05-14 14:04:37 +0100

    Remove deprecated secret_token config setting

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Bba::Application.config.secret_token = 'ad73feb3c4a1210527fa4be5230bb5c6e418328cefc1a58f7968ac098f210393e62764ec2f128bd97085f5bcd86fb94b5391e59f8e458bcb6721e6385822bdd8'
